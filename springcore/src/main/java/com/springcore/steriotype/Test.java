e4ab54c6bbbc0e52e7cf337ec6b6605b8d6f70ab
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2024-03-05 16:22:36 +0530

    bean scopes using annotation and using xml

--- springcore/src/main/java/com/springcore/steriotype/Test.java
+++ springcore/src/main/java/com/springcore/steriotype/Test.java
@@ -10,10 +10,26 @@ public class Test {
 		// TODO Auto-generated method stub
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/steriotype/sterioconfig.xml");
-        
+		// Prototype Scope using annotation
 		Student student = context.getBean("ob",Student.class);
 		System.out.println(student);
         System.out.println(student.getAddress());
+        
+        
+        System.out.println(student.hashCode());
+        
+        Student student1 = context.getBean("ob",Student.class);
+        System.out.println(student1.hashCode()); 
+        
+        
+        
+        
+        //Prototype Scope using bean
+        
+        Teacher teacher = context.getBean("teacher",Teacher.class);
+        System.out.println(teacher.hashCode()); 
+        Teacher teacher1 = context.getBean("teacher",Teacher.class);
+        System.out.println(teacher1.hashCode());
 	}
 
 }
