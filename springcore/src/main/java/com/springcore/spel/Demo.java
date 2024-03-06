fe26d04c5ea71e1e1dd603c4c315fce257d91048
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2024-03-06 08:23:09 +0530

    how to use static variable and methods in Expression langauge code

--- springcore/src/main/java/com/springcore/spel/Demo.java
+++ springcore/src/main/java/com/springcore/spel/Demo.java
@@ -11,6 +11,42 @@ public class Demo {
 	@Value("#{33*2}")
 	private int y;
 	
+	@Value("#{T(java.lang.Math).sqrt(25)}")
+	private double z;
+	
+	@Value("#{T(java.lang.Math).PI}")
+	private double d;
+	
+	@Value("#{new java.lang.String('Sameer Dhumal')}")
+	private String name;
+	
+	@Value("#{8-1>5}")
+	private boolean isActive;
+	
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getD() {
+		return d;
+	}
+
+	public void setD(double d) {
+		this.d = d;
+	}
+
 	public int getX() {
 		return x;
 	}
@@ -31,9 +67,20 @@ public class Demo {
 		// TODO Auto-generated constructor stub
 	}
 
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
 	@Override
 	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + "]";
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", d=" + d + ", name=" + name + ", isActive=" + isActive
+				+ "]";
 	}
+
+	
 
 }
